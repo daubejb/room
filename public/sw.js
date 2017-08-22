4c1f81201ae726c04f79916ff1781ef4d71dae2a
Jeffrey B. Daube <daubejb@gmail.com>
2017-08-22 14:53:45 -0400

    add room name, start time, and end time to confirmation daube dialog

--- public/sw.js
+++ public/sw.js
@@ -28,7 +28,7 @@ const fileManifest = [
   },
   {
     "url": "elements/daube-dialog/daube-dialog.html",
-    "revision": "93fc9fa522348a41cbe6f332f280bad2"
+    "revision": "a3756b5d4d3d9838a347af3e6f16b68f"
   },
   {
     "url": "elements/daube-gcal-api/daube-gcal-api.html",
@@ -52,7 +52,7 @@ const fileManifest = [
   },
   {
     "url": "elements/room-home.html",
-    "revision": "6e94d2bf2fe6222b914fc87d55055c37"
+    "revision": "ea0c1099ac3e12fd7b01a83dec76df94"
   },
   {
     "url": "elements/shared.css",
